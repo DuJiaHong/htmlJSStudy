40f44ac426d3e6abb57f5c510dde6be2879e3ff6
DuJiaHong <410254835@qq.com>
2021-11-18 14:19:27 +0800

    Create boxCloseTimer.java

---
+++ boxCloseTimer.java
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+		<style>
+			div {
+				width: 100px;
+				height: 100px;
+				position: fixed;
+				bottom: 0;
+				right: 0;
+				border: 1px solid #333;
+			}
+		</style>
+	</head>
+	<body>
+		
+		<div id="test1">
+			<button id="btn">x</button>
+		</div>
+		<script>
+			test1.style.backgroundColor = "#FF0FF0"
+			btn.onclick  = function () {
+				test1.style.display = "none"
+				setTimeout(function (){
+					test1.style.display = "block"
+				},1000)
+			}
+		</script>
+	</body>
+</html>
